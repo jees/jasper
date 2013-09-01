098550206f729a4c537898d958905ccb19239353
Lucas <smokin2005@gmail.com>
2013-09-01 15:19:25 -0500

    Added the application

---
+++ src/org/jasper/Application.java
@@ -0,0 +1,19 @@
+package org.jasper;
+
+/**
+ * The main entry point for our server application
+ * @author Ares_
+ *
+ */
+public final class Application {
+
+	/**
+	 * The entry point for our server application
+	 * @param args The program arguments
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
