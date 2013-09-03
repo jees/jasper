607d64bba5e511c9088a671b60665a3bac0241d6
Lucas <smokin2005@gmail.com>
2013-09-02 21:00:13 -0500

    GameConstants added
    Add anything that is related to the game that wont change.

---
+++ src/org/jasper/rs2/GameConstants.java
@@ -0,0 +1,32 @@
+package org.jasper.rs2;
+
+/**
+ * Handles all the game related constants
+ * @author Ares_
+ *
+ */
+public final class GameConstants {
+
+	/**
+	 * The port our game-server will be listening on
+	 */
+	public static final int PORT = 43594;
+	
+	/**
+	 * The revision of our game protocol
+	 */
+	public static final short REVISION = 317;
+	
+	/**
+	 * The game cycle delay
+	 */
+	public static final short CYCLE_RATE = 600;
+	
+	/**
+	 * The default private constructor
+	 */
+	private GameConstants() {
+		
+	}
+	
+}
