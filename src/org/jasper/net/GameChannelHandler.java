2d8feb6d24006a3a8c9d3de6051f7d71da4fa7c2
Lucas <smokin2005@gmail.com>
2013-09-04 21:30:50 -0500

    Start of Game channel handler

---
+++ src/org/jasper/net/GameChannelHandler.java
@@ -0,0 +1,50 @@
+package org.jasper.net;
+
+import java.util.logging.Logger;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
+
+/**
+ * 
+ * @author Ares_
+ *
+ */
+public final class GameChannelHandler extends SimpleChannelUpstreamHandler {
+
+	/**
+	 * The {@link Logger} instance for the {@link GameChannelHandler} class.
+	 */
+	private Logger logger = Logger.getLogger(GameChannelHandler.class.getName());
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.netty.channel.SimpleChannelUpstreamHandler#channelDisconnected(org.jboss.netty.channel.ChannelHandlerContext, org.jboss.netty.channel.ChannelStateEvent)
+	 */
+	@Override
+	public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent evt) {
+		
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.netty.channel.SimpleChannelUpstreamHandler#exceptionCaught(org.jboss.netty.channel.ChannelHandlerContext, org.jboss.netty.channel.ExceptionEvent)
+	 */
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent evt) {
+		
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.jboss.netty.channel.SimpleChannelUpstreamHandler#messageReceived(org.jboss.netty.channel.ChannelHandlerContext, org.jboss.netty.channel.MessageEvent)
+	 */
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent evt) throws Exception {
+	
+	}
+	
+}
